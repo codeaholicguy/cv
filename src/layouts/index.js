0ea29a9da4b4848f17878bfb3261f382222f26a6
Hoang Nhat Nguyen <hoangnn93@gmail.com>
2018-03-05 17:40:25 +0700

    Update components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
+import data from '../data/cv.json'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+export default class Layout extends Component {
+  static propTypes = {
+    children: PropTypes.func
+  }
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  static childContextTypes = {
+    data: PropTypes.object
+  }
+
+  getChildContext() {
+    return {data}
+  }
+
+  render() {
+    const {children} = this.props
+
+    return (
+      <div>
+        <Helmet
+          title={data.meta.title}
+          meta={[
+            {name: 'description', content: data.meta.description},
+            {name: 'keywords', content: data.meta.keywords}
+          ]}
+        />
+        <div>{children()}</div>
+      </div>
+    )
+  }
 }
-
-export default TemplateWrapper
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,115 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import PropTypes from 'prop-types'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = ({}, context) => {
+  const {info, summary, awards, history} = context.data
+
+  return (
+    <div>
+      <div>
+        <h1>{info.name}</h1>
+        <p>
+          <span>{info.mobile}</span>
+          <span> · </span>
+          <span>
+            <a href={info.email}>{info.email}</a>
+          </span>
+        </p>
+        <p>
+          <span>
+            <a href={info.linkedin}>{info.linkedin}</a>
+          </span>
+          <span> · </span>
+          <span>
+            <a href={info.github}>{info.github}</a>
+          </span>
+          <span> · </span>
+          <span>
+            <a href={info.blog}>{info.blog}</a>
+          </span>
+        </p>
+      </div>
+      <div>
+        <h2>Career summary</h2>
+        <ul>
+          {summary.map((item, index) => {
+            return <li key={`summary-${index}`}>{item}</li>
+          })}
+        </ul>
+      </div>
+      <div>
+        <h2>Awards</h2>
+        <ul>
+          {awards.map((item, index) => {
+            return (
+              <li key={`award-${index}`}>
+                <span>{item.year}</span>
+                <span> – </span>
+                <span>{item.detail}</span>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div>
+        <h2>Work history</h2>
+        <div>
+          {history.map((item, index) => {
+            return (
+              <div>
+                <h3>
+                  <span>{item.title}</span>
+                  <span>, </span>
+                  <span>{item.company}</span>
+                </h3>
+                <p>
+                  <span>
+                    <i>{item.time}</i>
+                  </span>
+                </p>
+                <p>
+                  <span>
+                    <strong>Skill: </strong>
+                  </span>
+                  <span>
+                    <i>{item.skills}</i>
+                  </span>
+                </p>
+                <div>
+                  {item.projects.map((project, index) => {
+                    return (
+                      <div>
+                        <p>{project.description}</p>
+                        <ul>
+                          {project.responsibilities.map(
+                            (responsibility, index) => {
+                              return <li>{responsibility}</li>
+                            }
+                          )}
+                        </ul>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div>
+        <h3>Education</h3>
+        <ul>
+          {summary.map((item, index) => {
+            return <li key={`summary-${index}`}>{item}</li>
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+IndexPage.contextTypes = {
+  data: PropTypes.object
+}
 
 export default IndexPage
